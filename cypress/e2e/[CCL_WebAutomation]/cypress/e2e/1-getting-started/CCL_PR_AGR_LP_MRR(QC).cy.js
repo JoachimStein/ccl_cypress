62ee23582450f41b3cb538cf0085bd04a98192ed
JoachimStein <steinjoachim19@gmail.com>
2024-03-10 16:58:55 +0600

    'FPO LPO'

---
+++ cypress/e2e/[CCL_WebAutomation]/cypress/e2e/1-getting-started/CCL_PR_AGR_LP_MRR(QC).cy.js
@@ -0,0 +1,160 @@
+describe("mrr(qc)_testsuite", function(){
+    it("process_flow_LP", function(){
+        // Define View
+        cy.viewport(1260,720)
+
+        // Login
+        cy.visit("http://192.168.3.187:7071/web/login") 
+        cy.get("#login").type("qa_user")
+        cy.wait(1000)
+        cy.get("#password").type("1234")
+        cy.wait(500)
+        cy.get(".btn").click()
+        cy.xpath("//i[@class='oi oi-apps']").click()
+        cy.xpath("//a[contains(.,'Purchase')]").click()
+        cy.wait(1000)
+
+        // Purchase Requisition
+        cy.xpath("(//button[contains(.,'New')])[2]").click()
+        cy.wait(1000)
+        cy.get("#requisition_type_0").select("Local Purchase")
+        cy.wait(1000)
+        cy.get("#budget_id_0").type("E")
+        cy.get("#budget_id_0").type('{Enter}')
+        cy.wait(1000)
+        cy.xpath("//a[contains(.,'Add a line')]").click()
+        cy.wait(1000)
+        cy.get("table[class='o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped']>tbody>tr:first-child>td:first-child").type("Confidence Cement")
+        cy.wait(1000)
+        cy.get("table[class='o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped']>tbody>tr:first-child>td:nth-child(5)").type("2")
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Submit')]").should('have.text','Submit')
+        cy.xpath("//span[contains(.,'Cancel')]").should('have.text','Cancel')
+        cy.xpath("//span[contains(.,'Submit')]").click()
+        cy.wait(1000)
+        cy.xpath("(//span[contains(.,'Approve')])[1]").should('have.text','Approve')
+        cy.xpath("(//span[contains(.,'Cancel')])").should('have.text','Cancel')
+        cy.xpath("(//span[contains(.,'Approve')])[1]").click()
+        cy.wait(1000)
+        cy.xpath("(//span[contains(.,'Approve')])[1]").should('have.text','Approve')
+        cy.xpath("(//span[contains(.,'Cancel')])").should('have.text','Cancel')
+        cy.xpath("(//span[contains(.,'Approve')])[1]").click()
+        cy.wait(1000)
+        cy.xpath("(//span[contains(.,'Amendment')])[1]").should('have.text','Amendment')
+        cy.xpath("(//span[contains(.,'Close')])").should('have.text','Close')
+
+        // Agreement Base
+
+        cy.xpath("//span[contains(.,'Orders')]").click()
+        cy.wait(1000)
+        cy.xpath("//a[contains(.,'Agreeement')]").click() // To-Do: `Agreement` ~ Spelling Error
+        cy.wait(1000)
+        cy.xpath("(//button[contains(.,'New')])[2]").click()         //New 
+        cy.wait(1000)
+        cy.get("#vendor_id_0").type("Local Vendor 1").type('{Enter}')
+        cy.wait(1000)
+        cy.get("#start_date_0").click()
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'10')]").click()
+        cy.wait(1000)
+        cy.get("#end_date_0").click()
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'11')]").click()
+        cy.wait(1000)
+        cy.get("#active_0").type('{Enter}')
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Confirm')]").should("have.text","Confirm")
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Confirm')]").click()
+        cy.wait(1000)        
+
+        // Local Purchase Order
+
+        cy.xpath("//span[contains(.,'Orders')]").click()
+        cy.wait(1000)
+        cy.xpath("//a[contains(.,'Local Purchase')]").click()
+        cy.wait(2000)
+        cy.xpath("(//button[contains(.,'New')])[2]").click()         //New 
+        cy.wait(1000)
+        cy.get("#purchase_base_0").select('Agreement')    //select
+        cy.wait(1000)
+        cy.get("#partner_id_0").type('Local Vendor 1').type('{Enter}')
+        cy.wait(1000)
+        cy.get("#agreement_id_0").type("VA00")
+        cy.wait(500)
+        cy.get("#agreement_id_0").type('{Enter}')
+        cy.wait(1000)
+        cy.get("#purchase_request_id_0").click()
+        cy.wait(500)
+        cy.get("#purchase_request_id_0").type('{Enter}')
+        cy.wait(1000)
+        cy.get("table[class='o_section_and_note_list_view o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped']>tbody>tr:first-child>td:nth-child(7)").type('2')
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Submit')]").click()
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Confirm Order')]").click()
+        cy.xpath("(//span[contains(.,'Amendment')])[1]").should('have.text','Amendment')
+        cy.xpath("//span[contains(.,'Close')]").should('have.text', 'Close')
+
+        // MRR with QC
+
+        // MRR
+
+        cy.wait(1000)
+        cy.xpath("//i[@class='oi oi-apps']").click()           //menu bar
+        cy.xpath("//a[contains(.,'Inventory')]").click()        //Inventory
+        cy.xpath("//span[contains(.,'Operations')]").click()    // dropdown
+        cy.wait(1000)
+        cy.xpath("(//a[contains(.,'MRR')])[1]").click()    // MRR select 
+        cy.wait(2000)
+        cy.xpath("(//button[contains(.,'New')])[2]").click()         //New 
+        cy.wait(1000)
+        cy.get("#mrr_type_0").select('Local')         //MRR ID
+        cy.get("#purchase_order_id_0").type('LPO/00')
+        cy.wait(1000)
+        cy.get("#purchase_order_id_0").type('{Enter}')   //PO id select
+        cy.wait(1000)
+        cy.xpath("//input[@id='qc_required_0']").click()
+        cy.wait(2000)
+        cy.xpath("//span[contains(.,'Send For Review')]").click()        
+        cy.wait(1000)
+
+        //  QC
+
+        cy.xpath("(//span[contains(.,'Operations')])[1]").click()    // dropdown
+        cy.wait(1000)
+        cy.xpath("//a[contains(.,'QC')]").click()    // MRR select 
+        cy.wait(2000)
+        cy.xpath("(//button[contains(.,'New')])[2]").click()         //New 
+        cy.wait(1000)
+        cy.get("#mrr_id_0").type("MRR").type('{Enter}')
+        cy.wait(1000)
+        cy.get("table[class='o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped']>tbody>tr:first-child>td:nth-child(5)").type("50")
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Send For Review')]").click()        
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Reviewed')]").should('have.text', 'Reviewed')
+        cy.xpath("(//span[contains(.,'Reviewed')])[1]").click()        
+        cy.wait(1000)
+        cy.xpath("(//span[contains(.,'Approved')])[1]").click()
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Closed')]").should('have.text','Closed')
+        cy.wait(1000)
+
+        // MRR
+        
+        cy.xpath("//span[contains(.,'Operations')]").click()    // dropdown
+        cy.wait(1000)
+        cy.xpath("(//a[contains(.,'MRR')])[1]").click()    // MRR select 
+        cy.wait(2000)
+        cy.get("table[class='o_list_table table table-sm table-hover position-relative mb-0 o_list_table_ungrouped table-striped']>tbody>tr:first-child>td:nth-child(3)").click()
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Reviewed')]").should('have.text', 'Reviewed')
+        cy.xpath("(//span[contains(.,'Reviewed')])[1]").click()        
+        cy.wait(1000)
+        cy.xpath("//span[contains(.,'Validate')]").click()        
+        cy.wait(1000)
+
+        Cypress.on('uncaught:exception', () => false)
+    })
+})
